12cd6b58d39b887d5ce05e668866a50e27e6175b
Jasper De Moor <jasperdemoor@gmail.com>
2021-10-02 20:49:23 +0200

    hack around garbage collection?

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const { Watcher } = require("./library");
 const path = require("path");
 
-class NodeWatcher {
-  // Options { ignores: string[] }
-  constructor(opts, callback) {
-    // The n-api watcher is a pretty basic watcher as we can call the event emitters
-    // from the node-side we just need a filtered stream of events from n-api
-    this.watcher = new Watcher({}, (err, ...values) => {
-      console.error(err);
-      console.log(values);
-    });
+async function run() {
+  class NodeWatcher {
+    // Options { ignores: string[] }
+    constructor(opts, callback) {
+      // The n-api watcher is a pretty basic watcher as we can call the event emitters
+      // from the node-side we just need a filtered stream of events from n-api
+      this.watcher = new Watcher({}, (err, ...values) => {
+        console.error(err);
+        console.log(values);
+      });
+    }
+
+    watch(p) {
+      this.watcher.watch(p);
+    }
+
+    unwatch(p) {
+      // TODO: Implement this
+      // this.watcher.unwatch(p);
+    }
+
+    updateIgnorePaths(paths) {
+      // TODO: Implement this
+    }
+
+    dispose() {
+      // TODO: Implement this
+      // this.watcher.dispose();
+    }
   }
 
-  watch(p) {
-    this.watcher.watch(p);
-  }
+  const w = new NodeWatcher();
+  w.watch(__dirname);
 
-  unwatch(p) {
-    // TODO: Implement this
-    // this.watcher.unwatch(p);
-  }
-
-  updateIgnorePaths(paths) {
-    // TODO: Implement this
-  }
-
-  dispose() {
-    // TODO: Implement this
-    // this.watcher.dispose();
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 1_000_000));
   }
 }
 
-const w = new NodeWatcher();
-w.watch(__dirname);
+run().catch(console.error);
